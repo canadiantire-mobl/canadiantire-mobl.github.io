95660e987cb404dd09756ac57ac2082d807922ac
Melvin Wong <melvin.wong@cantire.com>
2021-01-07 18:43:57 -0600

    added smooth scrolling to anchor click

--- js/theme.js
+++ js/theme.js
@@ -1,6 +1,6 @@
 AOS.init();
 
-var slider = tns({
+const slider = tns({
   container: '.team-members-list',
   items: 1,
   responsive: {
@@ -27,4 +27,33 @@ function navOpenClose() {
   } else {
     x.className = "";
   }
+}
+
+// Smooth scrolling polyfill
+if (!("scrollBehavior" in document.documentElement.style) && !!seamless) {
+  seamless.polyfill();
+
+  const scrollToSelector = selector => {
+    seamless.elementScrollIntoView(document.querySelector(selector), {
+      behavior: "smooth"
+    });
+  }
+
+  //scroll if pageload has hash
+  if (window.location.hash) {
+    scrollToSelector(window.location.hash);
+  }
+
+  //scroll on anchor click
+  const anchorClickHandler = function(event) {
+    if (event.target && event.target.hash) {
+      event.preventDefault();
+      scrollToSelector(event.target.hash);
+    }
+  }
+
+  Array.prototype.forEach.call(document.getElementsByTagName('a'), element => {
+    element.addEventListener('click', anchorClickHandler);
+  });
+
 }
